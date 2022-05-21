37bd27caa6edfce1ab08742b9cc5e35f663895f8
Pushpendra Kukreti <41129279+pushpendrakukreti@users.noreply.github.com>
2022-05-21 06:09:46 +0530

    created todo app using ReactJS

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
     return(
         <div className="navbar">
-            <h1>React To-Do <span>App</span></h1>
+            <h1>To-Do <span>App</span></h1>
         </div>
     )
 }
